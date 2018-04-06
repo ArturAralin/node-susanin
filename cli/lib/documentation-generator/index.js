583c0edb15e9230c1fc67b1b12c7797fe4f5adfb
Artur <artur.aralin97@gmail.com>
2018-04-06 12:43:31 +0300

    update cli
    add description for build-docs command
    change command params

--- cli/lib/documentation-generator/index.js
+++ cli/lib/documentation-generator/index.js
@@ -5,29 +5,53 @@ const path = require('path');
 const glob = require('glob');
 const getModel = require('./routes-ast');
 const docsGenerators = require('./generators');
+const {
+  printText,
+} = require('./generators/tools');
 
 const CWD = process.cwd();
 const DEFAULT_OUTPUT_FOLDER = path.resolve(CWD, './documentation');
-const DEFAULT_ROUTER_FILE_MASK = './**/*.router.js';
+
+const descriptionText = `Params specification:
+
+    <routes-mask> - Mask for routes files searching (example: path/to/**/*.router.js)
+    <docs-format> - Output documentation format
+
+  Supported docs format:
+    apidoc - Simple inline docs for RESTful api
+`;
 
 module.exports = (cli) => {
   cli
-    .command('build-docs <routes-directory> <docs-format>')
-    .option('--routerFileMask <routerFileMask>', 'Router files mask', DEFAULT_ROUTER_FILE_MASK)
-    .option('-O, --outputPath <outputPath>', 'xxx', DEFAULT_OUTPUT_FOLDER)
-    .action((routesDirectory, docsFormat, { outputPath, routerFileMask }) => {
-      const absoluteRoutesDirectory = path.resolve(CWD, routesDirectory);
+    .command('build-docs <routes-mask> <docs-format>')
+    .description(descriptionText)
+    .option('-O, --outputPath <outputPath>', 'path to output docs files', DEFAULT_OUTPUT_FOLDER)
+    .option('--verbose', 'verbose mode')
+    .action((routesMask, docsFormat, { outputPath, verbose }) => {
       const absoluteOutputPath = path.resolve(CWD, outputPath);
       const options = {
         realpath: true,
-        cwd: absoluteRoutesDirectory,
+        cwd: CWD,
       };
 
-      const ast = glob
-        .sync(routerFileMask, options)
+      const foundedFiles = glob.sync(routesMask, options);
+
+      if (foundedFiles.length === 0) {
+        printText('No router files found');
+
+        return;
+      }
+
+      if (verbose) {
+        printText(foundedFiles
+          .map(line => `(finded file) ${line}`)
+          .join('\n'));
+      }
+
+      const ast = foundedFiles
         .map(require)
         .map(getModel)
-        .reduce(concat);
+        .reduce(concat, []);
 
       docsGenerators[docsFormat](absoluteOutputPath, ast);
     });
